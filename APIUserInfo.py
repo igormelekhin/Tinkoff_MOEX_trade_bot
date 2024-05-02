0d15b7f2300ade3970d35a618f824613b0b4a34f
abc2 <mig1737@mail.ru>
2024-05-02 12:47:39 +0300

    Extraction of API token and account IDs from environment variables

---
+++ APIUserInfo.py
@@ -0,0 +1,47 @@
+import platform
+import os
+#TINKOFF_INVEST_API_TOKEN
+#TINKOFF_INVEST_ACC-ID-1
+#TINKOFF_INVEST_ACC-ID-2
+#APPNAME
+
+def printEnvironHelp():
+  currentOS = platform.system().lower()
+  if currentOS == "linux" or currentOS == "darwin":
+    print("\n\nRun in Bash:\nexport TINKOFF_INVEST_API_TOKEN = <your_token>\nexport TINKOFF_INVEST_ACC-ID-1 = <your_account_id>\n\n")
+  elif currentOS == 'windows':
+    print("\n\nRun in Cmd:\nset TINKOFF_INVEST_API_TOKEN = ‹your_token>\nset TINKOFF_INVEST_ACC-ID-1 = <your_account_id>\n\n")
+  print("Or run in Python:\nos.environ[\"TINKOFF_INVEST_API_TOKEN\"] = <your_token>\nos.environ[\"TINKOFF_INVEST_ACC-ID-1\"] = <your_account_id>\n\n")
+
+def getToken():
+  if "TINKOFF_INVEST_API_TOKEN" in os.environ:
+    return os.environ["TINKOFF_INVEST_API_TOKEN"]
+  else:
+    print("!!! Error - set environment variables \"TINKOFF_INVEST_API_TOKEN\" and \"TINKOFF_INVEST_ACC-ID-1\"")
+    printEnvironHelp()
+    return ""
+
+def getAppname():
+  if "TINKOFF_APPNAME" in os.environ:
+    return os.environ["TINKOFF_APPNAME"]
+  else:
+    return ""
+
+def getAccountIDs():
+  accountIDs = []
+  if "TINKOFF_INVEST_ACC-ID-1" in os.environ:
+    accountIDs.append(os.environ["TINKOFF_INVEST_ACC-ID-1"])
+  else:
+    print("!!! Error - set environment variables \"TINKOFF_INVEST_API_TOKEN\" and \"TINKOFF_INVEST_ACC-ID-1\"")
+    printEnvironHelp()
+    return []
+  if "TINKOFF_INVEST_ACC-ID-2" in os.environ:
+    accountIDs.append(os.environ["TINKOFF_INVEST_ACC-ID-2"])
+  return accountIDs
+
+
+
+if __name__ == "__main__":
+	print(getToken())
+	
+	
